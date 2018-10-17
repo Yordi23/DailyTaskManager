cba14818529e0f6b501114ffb036f4c998471a76
Jaydotcom27 <39386502+Jaydotcom27@users.noreply.github.com>
2018-10-17 11:21:22 -0400

    Revert "Revert "Plugin Install""
    This reverts commit dd8b54ada27b65bae144942382ccd9696f5987a2.

--- DailyTaskManager/DailyTaskManager.Android/Resources/Resource.designer.cs
+++ DailyTaskManager/DailyTaskManager.Android/Resources/Resource.designer.cs
@@ -26,7 +26,6 @@ namespace DailyTaskManager.Droid
 		
 		public static void UpdateIdValues()
 		{
-			global::Plugin.LocalNotifications.Resource.Drawable.plugin_lc_smallicon = global::DailyTaskManager.Droid.Resource.Drawable.plugin_lc_smallicon;
 			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::DailyTaskManager.Droid.Resource.Attribute.actionBarSize;
 			global::XLabs.Forms.Droid.Resource.Animation.abc_fade_in = global::DailyTaskManager.Droid.Resource.Animation.abc_fade_in;
 			global::XLabs.Forms.Droid.Resource.Animation.abc_fade_out = global::DailyTaskManager.Droid.Resource.Animation.abc_fade_out;
@@ -3937,26 +3936,26 @@ namespace DailyTaskManager.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
+			// aapt resource value: 0x7f02012f
+			public const int avd_hide_password_1 = 2130837807;
+			
 			// aapt resource value: 0x7f020130
-			public const int avd_hide_password_1 = 2130837808;
+			public const int avd_hide_password_2 = 2130837808;
 			
 			// aapt resource value: 0x7f020131
-			public const int avd_hide_password_2 = 2130837809;
-			
-			// aapt resource value: 0x7f020132
-			public const int avd_hide_password_3 = 2130837810;
+			public const int avd_hide_password_3 = 2130837809;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
+			// aapt resource value: 0x7f020132
+			public const int avd_show_password_1 = 2130837810;
+			
 			// aapt resource value: 0x7f020133
-			public const int avd_show_password_1 = 2130837811;
+			public const int avd_show_password_2 = 2130837811;
 			
 			// aapt resource value: 0x7f020134
-			public const int avd_show_password_2 = 2130837812;
-			
-			// aapt resource value: 0x7f020135
-			public const int avd_show_password_3 = 2130837813;
+			public const int avd_show_password_3 = 2130837812;
 			
 			// aapt resource value: 0x7f020056
 			public const int baseline_access_time_24 = 2130837590;
@@ -4567,11 +4566,11 @@ namespace DailyTaskManager.Droid
 			// aapt resource value: 0x7f020120
 			public const int notification_icon_background = 2130837792;
 			
-			// aapt resource value: 0x7f02012e
-			public const int notification_template_icon_bg = 2130837806;
+			// aapt resource value: 0x7f02012d
+			public const int notification_template_icon_bg = 2130837805;
 			
-			// aapt resource value: 0x7f02012f
-			public const int notification_template_icon_low_bg = 2130837807;
+			// aapt resource value: 0x7f02012e
+			public const int notification_template_icon_low_bg = 2130837806;
 			
 			// aapt resource value: 0x7f020121
 			public const int notification_tile_bg = 2130837793;
@@ -4586,31 +4585,28 @@ namespace DailyTaskManager.Droid
 			public const int Perfils = 2130837796;
 			
 			// aapt resource value: 0x7f020125
-			public const int plugin_lc_smallicon = 2130837797;
+			public const int settingsx = 2130837797;
 			
 			// aapt resource value: 0x7f020126
-			public const int settingsx = 2130837798;
+			public const int Splash = 2130837798;
 			
 			// aapt resource value: 0x7f020127
-			public const int Splash = 2130837799;
+			public const int Tiempo = 2130837799;
 			
 			// aapt resource value: 0x7f020128
-			public const int Tiempo = 2130837800;
+			public const int timec = 2130837800;
 			
 			// aapt resource value: 0x7f020129
-			public const int timec = 2130837801;
+			public const int timex = 2130837801;
 			
 			// aapt resource value: 0x7f02012a
-			public const int timex = 2130837802;
+			public const int UserBig = 2130837802;
 			
 			// aapt resource value: 0x7f02012b
-			public const int UserBig = 2130837803;
+			public const int userc = 2130837803;
 			
 			// aapt resource value: 0x7f02012c
-			public const int userc = 2130837804;
-			
-			// aapt resource value: 0x7f02012d
-			public const int xamarin_logo = 2130837805;
+			public const int xamarin_logo = 2130837804;
 			
 			static Drawable()
 			{
